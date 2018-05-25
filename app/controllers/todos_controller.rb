9a827f81e9af224b9e9d2cf423be7c61b0dec47c
simplay <silent.simplay@gmail.com>
2018-05-25 23:54:24 +0200

    Implement create todo endpoint

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -12,4 +12,15 @@ class TodosController < ApplicationController
       render json: {}, staus: 404
     end
   end
+
+  def create
+    todo_params = params[:todo]
+    todo = Todo.new(title: todo_params["title"])
+    if todo.save!
+      render json: Todo.all, status: 200
+    else
+      render json: {}, staus: 404
+    end
+
+  end
 end
